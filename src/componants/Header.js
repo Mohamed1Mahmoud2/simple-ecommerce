88b9eff4b768bb54f29580a76567b5bc93ed3f38
Mohamed Mahmoud <103222555+Mohamed1Mahmoud2@users.noreply.github.com>
2024-10-30 11:47:57 +0300

    update

---
+++ src/componants/Header.js
@@ -0,0 +1,21 @@
+function Header() {
+    return (
+        <header>
+            <nav className="navgation">
+                <ul className="catg">
+                    <li>watch</li>
+                    <li>phone</li>
+                    <li>lap</li>
+                    <li>tv</li>
+                </ul>
+            </nav>
+            <div>
+                <h1>Smart Store</h1>
+
+                <p>You can find your watch, phone, lap and tv here</p>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
