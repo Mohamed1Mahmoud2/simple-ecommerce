f03dc297cb8eee9dfcb60816bf74fb7756329729
Mohamed Mahmoud <103222555+Mohamed1Mahmoud2@users.noreply.github.com>
2024-10-31 14:59:05 +0300

    add footer

---
+++ src/componants/Footer.js
@@ -0,0 +1,14 @@
+
+function Footer() {
+    return (
+        <footer className="footer-container  chokokutai-regular">
+            <div>
+                <p>Developed by Mohamed Mahmoud</p>
+                <p>Mail</p>
+                <p>linkedin</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
