fdefd385a80aa7f62d04b706978f45d46f16a00c
Mohamed Mahmoud <103222555+Mohamed1Mahmoud2@users.noreply.github.com>
2024-11-02 17:05:45 +0200

    update footer

--- src/componants/Footer.js
+++ src/componants/Footer.js
@@ -1,11 +1,15 @@
+import { FaLinkedinIn, FaGithub, FaGoogle, FaCode } from 'react-icons/fa'
 
 function Footer() {
     return (
         <footer className="footer-container  chokokutai-regular">
             <div>
-                <p>Developed by Mohamed Mahmoud</p>
-                <p>Mail</p>
-                <p>linkedin</p>
+                <p>Developed by Mohamed Mahmoud  <FaCode /></p>
+                <p>GitHub  <FaGithub /></p>
+                <p>linkedin  < FaLinkedinIn /></p>
+                <p>Mail  <FaGoogle /></p>
+
+
             </div>
         </footer>
     )
